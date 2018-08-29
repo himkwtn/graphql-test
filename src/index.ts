9bbe808acea1b21dba66fe2ac388e1659893ab4a
Watcharin Kriengwatana <him10424@gmail.com>
2018-08-29 21:49:24 +0700

    test port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const typeDefs = `
   }
 
   type Mutation {
-    createStudent(name: String,contact: ContactInput ,courses:[String]): Student!
+    createStudent(name: String,contact: ContactInput, courses:[String]): Student!
   }
 
   type Student {
@@ -60,8 +60,8 @@ const findStudent = id => students.find( student => student.id == id)
 
 const update = student => {
   students.push(student)
-  console.log(students, students.length)
   const data = JSON.stringify(students, null, 4)
   writeFileSync('students.json', data)
 }
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.options.port = 4000
+server.start(() => console.log(`Server is running on http://localhost:${server.options.port}`))
