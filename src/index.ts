1a32d68868ee7ce02d2b118d0b833a759e87d3cc
Watcharin Kriengwatana <him10424@gmail.com>
2018-08-29 21:39:41 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import { GraphQLServer } from 'graphql-yoga'
+const students = require("./students")
+import {writeFileSync} from "fs"
+const typeDefs = `
+  type Query {
+    student(id: Int): Student 
+  }
+
+  type Mutation {
+    createStudent(name: String,contact: ContactInput ,courses:[String]): Student!
+  }
+
+  type Student {
+    id: Int
+    name: String
+    contact: Contact
+    courses: [String]
+  }
+
+  type Contact {
+    phone: Int
+    email: String
+    line: String
+  }
+
+  input ContactInput {
+    phone: Int
+    email: String
+    line: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    student: (_, {id}) => {
+      const s = id !== undefined ? findStudent(id) : students
+      return s
+    }
+  },
+  Mutation: {
+    createStudent:(_,{name,contact,courses}) => {
+      const student = {
+        id: students.length+1,
+        name,
+        contact,
+        courses
+      }
+      update(student)
+      return findStudent(student.id)
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+const findStudent = id => students.find( student => student.id == id)
+
+const update = student => {
+  students.push(student)
+  console.log(students, students.length)
+  const data = JSON.stringify(students, null, 4)
+  writeFileSync('students.json', data)
+}
+server.start(() => console.log('Server is running on http://localhost:4000'))
